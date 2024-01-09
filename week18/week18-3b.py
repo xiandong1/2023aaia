238129e857bced298c21da320246f6ddb83a8a50
xiandong1 <hg96041057@gmail.com>
2024-01-09 11:56:05 +0800

    week18

---
+++ week18/week18-3b.py
@@ -0,0 +1,4 @@
+a = int(input("請輸入數字"))
+b = int(input("請再輸入第二個數字"))
+ans = (a+b)
+print("答案是",ans)
---
+++ week18/week18-4a.py
@@ -0,0 +1,12 @@
+#Chapter 2 
+#a = int(input())
+a = int("12345677")
+print(a+999)
+
+#bool() - False True
+#int() 0 1 2 3
+#float() 0.1 0.2 0.3
+#str() "單雙引號"
+
+ans = type("12345")
+print(ans)
